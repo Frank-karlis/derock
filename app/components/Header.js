12767b0774294e3fc962fde9363419268cd761f4
Okoli Frank <okolicharles104@gmail.com>
2024-07-22 10:12:02 +0100

    design contact us

--- app/components/Header.js
+++ app/components/Header.js
@@ -8,10 +8,10 @@ export default function Header(){
                 <FaCar className="text-[#17153b] text-5xl"/>
                 <p className="text-[#17153b] text-2xl"> DE-ROCK MOTORS</p>
             </div> 
-            <div className="flex gap-5 justity-center">
-                <Link className="py-3 text-[#433d8b] text-3xs text-bold" href="/page.jsx">Home</Link>
-                <Link className="py-3 text-[#433d8b] text-3xs text-bold" href="/https://maps.app.goo.gl/X5UkYMTsRUuRAsF49?g_st=iw">Locate us</Link>
-                <Link className="py-3 text-[#433d8b] text-3xs text-bold" href="?">Home</Link>
+            <div className=" lg:flex gap-5 justity-center max-sm:hidden ">
+                <Link className="py-3 text-[#433d8b] text-3xs text-bold" href="/app">Home</Link>
+                <Link className="md:py-3 text-[#433d8b] text-3xs text-bold" href="https://maps.app.goo.gl/X5UkYMTsRUuRAsF49?g_st=iw">Locate us</Link>
+                <Link className="py-3 text-[#433d8b] text-3xs text-bold" href="/contact">Contact us</Link>
                 <Link className="py-3 text-[#433d8b] text-3xs text-bold" href="?">Home</Link>
             </div>
         </nav>
